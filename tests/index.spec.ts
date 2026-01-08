af7e15f769776c8df3c1af86b3e61ae582b7a093
Darren Griffin <darren.griffin@live.co.uk>
2026-01-08 12:12:15 +0000

    Make test pass

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("meta is correct", async ({ page }) => {
   await page.goto("http://localhost:4321/");
 
-  await expect(page).toHaveTitle("Astro fail Basics");
+  await expect(page).toHaveTitle("Astro Basics");
 });
