bf87be511bd86517c0f5f3337b406478b5b77674
Darren Griffin <darren.griffin@live.co.uk>
2026-01-08 10:13:04 +0000

    Remove 11ty, add Astro

--- _data/metadata.js
+++
@@ -1,6 +0,0 @@
-export default {
-	title: "Open Home Foundation Eleventy Starter",
-	url: "https://eleventy.openhomefoundation.org.io/",
-	description: "A starter template for Eleventy projects by Open Home Foundation.",
-	language: "en"
-}
