faed7646a3781f12bcb6fb3261d6bda7dafc6607
Darren Griffin <darren.griffin@live.co.uk>
2026-01-12 15:28:35 +0000

    Add better image sizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { imageBreakpoints } from "./src/lib/breakpoints";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    breakpoints: imageBreakpoints,
+    responsiveStyles: true,
+  },
+});
