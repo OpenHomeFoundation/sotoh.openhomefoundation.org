8587b9e95c2831d5888c8d5a60ac7289b058774d
Darren Griffin <darren.griffin@live.co.uk>
2026-01-27 16:59:57 +0000

    Add text overlays

--- src/lib/tickets.ts
+++ src/lib/tickets.ts
@@ -2,6 +2,7 @@ export interface Ticket {
   id: number;
   image: string;
   overlay: string;
+  text: string;
   theme: "light" | "dark";
 }
 
@@ -10,96 +11,112 @@ export const tickets: Ticket[] = [
     id: 1,
     image: "/images/ticket/ticket-1-dark.svg",
     overlay: "/images/ticket/ticket-1-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 2,
     image: "/images/ticket/ticket-1-light.svg",
     overlay: "/images/ticket/ticket-1-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 3,
     image: "/images/ticket/ticket-2-dark.svg",
     overlay: "/images/ticket/ticket-2-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 4,
     image: "/images/ticket/ticket-2-light.svg",
     overlay: "/images/ticket/ticket-2-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 5,
     image: "/images/ticket/ticket-3-dark.svg",
     overlay: "/images/ticket/ticket-3-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 6,
     image: "/images/ticket/ticket-3-light.svg",
     overlay: "/images/ticket/ticket-3-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 7,
     image: "/images/ticket/ticket-4-dark.svg",
     overlay: "/images/ticket/ticket-4-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 8,
     image: "/images/ticket/ticket-4-light.svg",
     overlay: "/images/ticket/ticket-4-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 9,
     image: "/images/ticket/ticket-5-dark.svg",
     overlay: "/images/ticket/ticket-5-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 10,
     image: "/images/ticket/ticket-5-light.svg",
     overlay: "/images/ticket/ticket-5-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 11,
     image: "/images/ticket/ticket-6-dark.svg",
     overlay: "/images/ticket/ticket-6-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 12,
     image: "/images/ticket/ticket-6-light.svg",
     overlay: "/images/ticket/ticket-6-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 13,
     image: "/images/ticket/ticket-7-dark.svg",
     overlay: "/images/ticket/ticket-7-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 14,
     image: "/images/ticket/ticket-7-light.svg",
     overlay: "/images/ticket/ticket-7-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
   {
     id: 15,
     image: "/images/ticket/ticket-8-dark.svg",
     overlay: "/images/ticket/ticket-8-overlay.svg",
+    text: "/images/ticket/text-on-dark.svg",
     theme: "dark",
   },
   {
     id: 16,
     image: "/images/ticket/ticket-8-light.svg",
     overlay: "/images/ticket/ticket-8-overlay.svg",
+    text: "/images/ticket/text-on-light.svg",
     theme: "light",
   },
 ];
