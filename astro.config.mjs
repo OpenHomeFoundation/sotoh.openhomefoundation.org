d6b963af44ca8134960585d2090e361cf5c135d0
Darren Griffin <darren.griffin@live.co.uk>
2026-01-29 10:17:10 +0000

    Add invite redirect and randomised OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 import { imageBreakpoints } from "./src/lib/breakpoints";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sotoh.openhomefoundation.org",
+  output: "static",
+  adapter: netlify(),
   integrations: [sitemap()],
   image: {
     breakpoints: imageBreakpoints,
