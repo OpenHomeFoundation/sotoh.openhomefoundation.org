9adaf059923010f06852309658e95975626a0c90
Darren Griffin <darren.griffin@live.co.uk>
2026-01-29 10:47:19 +0000

    Add credit

---
+++ src/lib/credits.ts
@@ -0,0 +1,7 @@
+export function showCredits() {
+  console.log(
+    "%cWant to see how we made the holographic ticket?",
+    "font-weight: bold",
+  );
+  console.log("\thttps://codepen.io/openhomefoundation/pen/ogLpqXJ");
+}
