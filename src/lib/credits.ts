aec254abe099d9930bf352486c63934eb3821de5
Darren Griffin <darren.griffin@live.co.uk>
2026-01-29 10:56:45 +0000

    Move credits functionality to external script and remove unused import

--- src/lib/credits.ts
+++
@@ -1,7 +0,0 @@
-export function showCredits() {
-  console.log(
-    "%cWant to see how we made the holographic ticket?",
-    "font-weight: bold",
-  );
-  console.log("\thttps://codepen.io/openhomefoundation/pen/ogLpqXJ");
-}
